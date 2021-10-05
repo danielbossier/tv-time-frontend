a9d8fe247e56aaf7355329405ef4979553a42484
danielbossier <dbossier21@gmail.com>
2021-10-05 10:09:08 -0700

    added index vue page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import ShowsIndex from "../views/ShowsIndex.vue";
+import ShowsNew from "../views/ShowsNew.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/ShowsIndex",
+    name: "ShowsIndex",
+    component: ShowsIndex,
+  },
+  {
+    path: "/ShowsNew",
+    name: "ShowsNew",
+    component: ShowsNew,
   },
   {
     path: "/about",
